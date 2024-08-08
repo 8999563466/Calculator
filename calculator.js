a00269312004ab8f5c89ad8c1ffc0e264282b423
Gauri Sudhir Lolage <141613563+8999563466@users.noreply.github.com>
2024-08-08 20:27:51 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,31 @@
+// script.js
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function backspace() {
+    const display = document.getElementById('display');
+    if (display.innerText.length === 1) {
+        display.innerText = '0';
+    } else {
+        display.innerText = display.innerText.slice(0, -1);
+    }
+}
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
